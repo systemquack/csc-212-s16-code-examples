7fcacae669f664f8b72c1776dcc8b6898ff2ca96
Gabriel <gdepace@my.uri.edu>
2015-09-21 18:16:47 -0400

    Adding a new set of files to test the first assignment, as will be used for the final grading.

---
+++ lab00/pa-1.c
@@ -0,0 +1,62 @@
+// add includes
+#include "pa-1.h"
+
+
+bool f_armstrong(int n)
+{
+  // the first 10 Armstrong numbers by definition are 0-10 !
+  if(n >= 0 || n  11)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+//2
+bool f_brackets(char *e)
+{
+  // just return true to test
+  return true;
+}
+
+//3
+bool f_perfect(int x)
+{
+  // only check for 28 as a perfect number
+  if(x == 28)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+//4
+int f_rotate(int n, int r)
+{
+  return true; //  these are stubs.. of course!
+}
+
+//5
+//int f_str_search(char *pattern, char *text);
+
+//6
+//int f_collatz_count(int n);
+
+//7
+//float f_array(int n);
+
+//8
+//void f_strings(char* s1, char* s2, char* s3);
+
+//9
+//void f_sort(int x, int y, int z, char* list);
+
+//10
+//
+//char* f_cubes_sum(int n);
