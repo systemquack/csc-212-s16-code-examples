8787d82d228292055e6993f60511af38d10aa22f
Marco <malvarezvg@gmail.com>
2015-09-22 13:42:56 -0400

    comments in C style

--- c-lectures/endianness/prog.c
+++ c-lectures/endianness/prog.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-// prints decimal and hexadecimal representation for an integer
+/* prints decimal and hexadecimal representation for an integer */
 void print_info(int v);
 
 int main (int argc, char **argv) {
     int var;
 
-    // iterate until the value of var is 0
+    /* iterate until the value of var is 0 */
     while(1) {
-        // read an integer from the user
+        /* read an integer from the user */
         printf("Enter a number (type 0 to exit): ");
         scanf("%d", &var);
         if (! var) break;
-        // inspect contents of var
+        /* inspect contents of var */
         print_info(var);
     }
 
@@ -20,16 +20,16 @@ int main (int argc, char **argv) {
 }
 
 void print_info(int v) {
-    // lets create a char * that points to v
-    // casting will be necessary because v is an int
+    /* lets create a char * that points to v
+       casting will be necessary because v is an int */
     unsigned char *p = (unsigned char *) &v;
 
-    // print deximal and hex representation
+    /* print deximal and hex representation */
     printf("Decimal: %d\tHexadecimal: 0x%08X\tBytes:", v, v);
-    // ... followed by the bytes in memory
+    /* ... followed by the bytes in memory */
     for (int i = 0 ; i < 4 ; i ++) {
         printf(" %02x", *(p+i));
     }
-    // new line character
+    /* new line character */
     printf("\n\n");
 }
