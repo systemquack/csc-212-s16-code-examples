54353ca27f192f77ca24140773fbd313c963e6d5
Marco <malvarezvg@gmail.com>
2015-10-01 11:08:00 -0400

    dyn arrays

---
+++ lectures/dynamic-arrays/dyn-array.cc
@@ -0,0 +1,68 @@
+#include "dyn-array.h"
+
+// both constructors assume that cap is greater than zero
+// you can also catch exceptions for allocation errors
+DynArray::DynArray(ul_int cap) {
+    Initialize(cap, 0.1);
+}
+
+DynArray::DynArray(ul_int c, float r) {
+    Initialize(c, r);
+}
+
+void DynArray:: Initialize(ul_int c, float r) {
+    size = 0;
+    n_resizes = 0;
+    capacity = c;
+    rate = r;
+    data = new ul_int [c];
+}
+
+DynArray::~DynArray() {
+    delete [] data;
+}
+
+// makes an insertion at the end of the array
+void DynArray::append(ul_int elem) {
+    // check if needs to grow the array
+    if (size == capacity) {
+        n_resizes ++;
+        // save pointer to array
+        ul_int *old = data;
+        // calculate new capacity
+        ul_int increase = (ul_int) capacity * rate;
+        capacity += (increase > 0) ? increase : 1;
+        // allocate space for new array
+        data = new ul_int [capacity];
+        // copy all elements
+        for (ul_int i = 0 ; i < size ; i ++) {
+            data[i] = old[i];
+        }
+        // delete old array
+        delete [] old;
+    }
+    // makes the insertion
+    data[size++] = elem;
+}
+
+ul_int DynArray::get_size() {
+    return size;
+}
+
+ul_int DynArray::get_capacity() {
+    return capacity;
+}
+
+int DynArray::get_n_resizes() {
+    return n_resizes;
+}
+
+float DynArray::get_rate() {
+    return rate;
+}
+
+// not checking for out-of-bounds here but ... could 
+// throw exceptions in such a case
+ul_int DynArray::get(ul_int i) {
+    return data[i];
+}
