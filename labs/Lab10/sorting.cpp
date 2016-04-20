5e355654b29519c3514e0a2e060e45b2b125e2c2
Marco <malvarezvg@gmail.com>
2016-04-20 14:10:13 -0400

    fix

--- labs/Lab10/sorting.cpp
+++ labs/Lab10/sorting.cpp
@@ -7,19 +7,38 @@
 
 typedef unsigned long int ul_int;
 
-void exchange(ul_int *x, ul_int *y) {
-    // add this code
+////////////////////////////////////////////////////////////////////////////////
+// Quick sort with 3-way partitioning
+////////////////////////////////////////////////////////////////////////////////
+void three_way_partition(ul_int *A, ul_int lo, ul_int hi, ul_int *p1, ul_int *p2) {
+    // your code here
 }
 
-void qsort3(ul_int *A, int lo, int hi) {
-
-    // add this code
+void r_three_way_quicksort(ul_int *A, ul_int lo, ul_int hi) {
+    // base case
+    if (hi <= lo) return;
+    // these are the two pivots that should be updated by three_way_partition
+    ul_int p1 = lo, p2 = hi;
+    // partition
+    three_way_partition(A, lo, hi, &p1, &p2);
+    // recursively sort halves
+    // also avoid negative indices because indices are unsigned int
+    if (p1 > 0) r_three_way_quicksort(A, lo, p1-1);
+    r_three_way_quicksort(A, p2+1, hi);
 }
 
-void quicksort3(ul_int *A, ul_int n) {
-    // add call to qsort3 ..
+void three_way_quicksort(ul_int *A, ul_int n) {
+    // shuffle the array
+    std::random_device rng;
+    std::mt19937 urng(rng());
+    std::shuffle(A, A+n, urng);
+    // call recursive quicksort
+    r_three_way_quicksort(A, 0, n-1);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Quick sort with 2-way partitioning
+////////////////////////////////////////////////////////////////////////////////
 ul_int partition(ul_int *A, ul_int lo, ul_int hi) {
     ul_int temp, i = lo, j = hi + 1;
     while (1) {
@@ -49,7 +68,8 @@ void r_quicksort(ul_int *A, ul_int lo, ul_int hi) {
     if (hi <= lo) return;
     // partition
     ul_int p = partition(A, lo, hi);
-    // recursively sort halves (also avoid negative indices)
+    // recursively sort halves
+    // also avoid negative indices because indices are unsigned int
     if (p > 0) r_quicksort(A, lo, p-1);
     r_quicksort(A, p+1, hi);
 }
@@ -63,9 +83,12 @@ void quicksort(ul_int *A, ul_int n) {
     r_quicksort(A, 0, n-1);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Merge sort
+////////////////////////////////////////////////////////////////////////////////
 void merge(ul_int *A, ul_int *aux, ul_int lo, ul_int mid, ul_int hi) {
     // copy array
-    std::memcpy(aux, A+lo, (hi-lo+1)*sizeof(ul_int));
+    std::memcpy(aux+lo, A+lo, (hi-lo+1)*sizeof(ul_int));
     // merge
     ul_int i = lo, j = mid + 1;
     for (ul_int k = lo ; k <= hi ; k ++) {
@@ -80,7 +103,7 @@ void r_mergesort(ul_int *A, ul_int *aux, ul_int lo, ul_int hi) {
     // base case
     if (hi <= lo) return;
     // divide
-    ul_int mid = (hi + lo) / 2;
+    ul_int mid = lo + (hi - lo) / 2;
     // recursively sort halves
     r_mergesort(A, aux, lo, mid);
     r_mergesort(A, aux, mid+1, hi);
@@ -97,22 +120,9 @@ void mergesort(ul_int *A, ul_int n) {
     delete [] aux;
 }
 
-void bubblesort(ul_int *A, ul_int n) {
-    ul_int temp, i, j;
-    // pass through the array n-1 times
-    for (i = 0 ; i < (n-1) ; i ++) {
-        // check for out-of-order pairs in the 'unsorted' half
-        for (j = 0 ; j < (n-i-1) ; j ++) {
-            // if A[j] and A[j+1] are out-of-order, swap them
-            if (A[j] > A[j+1]) {
-                temp = A[j+1];
-                A[j+1] = A[j];
-                A[j] = temp;
-            }
-        }
-    }
-}
-
+////////////////////////////////////////////////////////////////////////////////
+// Quadratic algorithms
+////////////////////////////////////////////////////////////////////////////////
 void insertionsort(ul_int *A, ul_int n) {
     ul_int temp, i, j;
     // grows the left part (sorted)
@@ -144,6 +154,9 @@ void selectionsort(ul_int *A, ul_int n) {
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Auxiliary functions
+////////////////////////////////////////////////////////////////////////////////
 void time_func(void (*f_ptr)(ul_int *, ul_int), ul_int *array, ul_int n, const char *name) {
     clock_t tic = clock();
     (*f_ptr)(array, n);
@@ -159,18 +172,28 @@ int is_sorted(ul_int *A, ul_int n) {
     return 1;
 }
 
-int main() {
-    const int n_algo = 6;
-    // array of function names
-    const char * fun_names[n_algo] = {"QuickSort", "MergeSort", "QuickSort3", "InsertionSort", "SelectionSort", "BubbleSort"};
-    // array of pointers to functions
-    void (*fun_ptrs[n_algo])(ul_int *, ul_int) = {&quicksort, &mergesort, &quicksort3, &insertionsort, &selectionsort, &bubblesort};
+void print_array(ul_int *array, char *name, int n) {
+    std::cout << name << ": ";
+    for (int i = 0 ; i < n ; i ++)
+        std::cout << array[i] << " ";
+    std::cout << "\n";
+}
 
-    std::cout << "Reading input sequence ...\n";
+////////////////////////////////////////////////////////////////////////////////
+// Main function
+////////////////////////////////////////////////////////////////////////////////
+int main() {
+    const int n_algo = 5;
+    // array of function names
+    const char * fun_names[n_algo] = {"3-Way QuickSort", "QuickSort", "MergeSort", "InsertionSort", "SelectionSort"};
+    // array of pointers to functions
+    void (*fun_ptrs[n_algo])(ul_int *, ul_int) = {&three_way_quicksort, &quicksort, &mergesort, &insertionsort, &selectionsort};
 
     // read first number
     ul_int n, i;
     std::cin >> n;
+
+    std::cout << "Reading input sequence of size " << n << " ...\n";
 
     // allocate space for arrays
     ul_int *array = new ul_int[n];
